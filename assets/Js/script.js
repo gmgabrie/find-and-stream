f212a4f1068402db7182e35f59e0b30690cae92e
Gdel-J <jessygdelvecchio@gmail.com>
2023-04-03 19:58:18 -0400

    Js file started

---
+++ assets/Js/script.js
@@ -0,0 +1,16 @@
+// global variables
+var apiKey = "";
+var savedSearches = [];
+
+
+
+
+// make list of previously searched movies
+
+// turn saved search history string into array
+
+ // get and use data from  movie API   
+
+ //with fetch
+
+ // reset search input
