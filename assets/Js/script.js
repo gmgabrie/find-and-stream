614edcf0c5e35636b88de581ced7b6a68e0d47db
gmgabrie <ggabriel420@gmail.com>
2023-04-04 19:27:24 -0400

    added function for call trending API and log data to console

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,4 +1,22 @@
 //Needed for Materialize
-$(document).ready(function() {
-    M.updateTextFields();
-  });
+// $(document).ready(function() {
+//     M.updateTextFields();
+//   });
+
+
+
+var trendingApiUrl = 'https://api.themoviedb.org/3/trending/movie/week?api_key='
+var trendingApiKey = '768690ea624c6d0cff681d2edcb833a2'
+
+function displayTrending() {
+  fetch(trendingApiUrl + trendingApiKey)
+  .then(function(response) {
+    return response.json();
+})
+  .then(function(data) {
+    console.log(data);
+  })
+}
+
+
+displayTrending();
