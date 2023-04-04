c1d971e40e3f8a72c46f8933f722636d197993cb
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-04-04 18:23:52 -0500

    add functionality to buttons on cards.

---
+++ script_watchlist_buttons.js
@@ -0,0 +1,26 @@
+
+// let watchListItem = document.getElementsByClassName('watchlist-item');
+let addBtn1 = document.getElementById('watchlist-btn1');
+let addBtn2 = document.getElementById('watchlist-btn2');
+let addBtn3 = document.getElementById('watchlist-btn3');
+let addBtn4 = document.getElementById('watchlist-btn4');
+let titleName = document.getElementsByClassName('title-name');
+
+
+function addToWatchlist() {
+    
+    // const watchList = document.getElementById('watchlist');
+    const newItem = document.createElement('a');
+    const newItemText = document.createTextNode(titleName);
+    newItem.appendChild(newItemText);
+    document.getElementById('watchlist').appendChild(newItem);
+    newItem.className = "collection-item"
+
+    console.log('event listener works!')
+
+}
+//Event Listeners
+addBtn1.addEventListener('click', addToWatchlist);
+addBtn2.addEventListener('click', addToWatchlist);
+addBtn3.addEventListener('click', addToWatchlist);
+addBtn4.addEventListener('click', addToWatchlist);
